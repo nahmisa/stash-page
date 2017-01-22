5825bc7bcb1524c490f3dcd4ad549435a15d5629
Sarah Nahmias <nahmisa@gmail.com>
2017-01-21 22:08:48 -0800

    Toggle dissapears but sort of works

--- app/controllers/milks_controller.rb
+++ app/controllers/milks_controller.rb
@@ -1,2 +1,17 @@
 class MilksController < ApplicationController
+  def create
+    @milk = Milk.new(milk_params)
+
+    if @milk.save
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def milk_params
+    params.require(:milk).permit(:milk_type, :amount, :location, :date)
+  end
 end
