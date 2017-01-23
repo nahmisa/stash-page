96c368a50b41e2824f9165f6304fa45be897d88e
Sarah Nahmias <nahmisa@gmail.com>
2017-01-23 13:30:28 -0800

    Comments out migration so it doesnt run on deploy

--- db/migrate/20170122042222_create_milks.rb
+++ db/migrate/20170122042222_create_milks.rb
@@ -1,14 +1,14 @@
 # encoding: utf-8
 
-class CreateMilks < ActiveRecord::Migration[5.0]
-  def change
-    create_table :milks do |t|
-      t.string   :location
-      t.integer  :amount
-      t.datetime :exp_date
-      t.datetime :date
-      t.string   :milk_type
-      t.timestamps
-    end
-  end
-end
+# class CreateMilks < ActiveRecord::Migration[5.0]
+#   def change
+#     create_table :milks do |t|
+#       t.string   :location
+#       t.integer  :amount
+#       t.datetime :exp_date
+#       t.datetime :date
+#       t.string   :milk_type
+#       t.timestamps
+#     end
+#   end
+# end
