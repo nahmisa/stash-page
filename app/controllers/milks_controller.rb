ea4055f168fa26ca3562fbf7bf5f942a9cfe0ab2
Sarah Nahmias <nahmisa@gmail.com>
2017-01-24 22:18:39 -0800

    Updates expiration date for different milk types

--- app/controllers/milks_controller.rb
+++ app/controllers/milks_controller.rb
@@ -1,11 +1,13 @@
 class MilksController < ApplicationController
   def create
     @milk = Milk.new(milk_params)
+    # matches /stash formatting of downcase
+    @milk.milk_type.downcase!
     @milk.location = "home"
-    @milk.exp_date = Time.now()
+    @milk.exp_date = @milk.get_exp_date(@milk.milk_type, @milk.date)
 
     @milk.save!
-
+    # will need to tackle error handling later
     respond_to do |format|
       format.html { redirect_to root_path }
       format.js # render milks/create.js.erb
@@ -15,6 +17,6 @@ class MilksController < ApplicationController
   private
   # Never trust parameters from the scary internet, only allow the white list through.
   def milk_params
-    params.require(:milk).permit(:milk_type, :amount, :date)
+    params.require(:milk).permit(:milk_type, :date, :amount)
   end
 end
--- app/models/milk.rb
+++ app/models/milk.rb
@@ -1,2 +1,24 @@
 class Milk < ApplicationRecord
+  validates :milk_type,
+    presence: true,
+    inclusion: { in: %w(fresh frozen thawed),
+      message: "%{value} is not a valid milk type" }
+  validates :date, presence: true
+  validates :amount, presence: true,
+    numericality: { only_integer: true }
+
+  def get_exp_date(milk_type, date)
+    #https://www.cdc.gov/breastfeeding/recommendations/handling_breastmilk.htm
+    # assuming for now that fresh means in the refridgerator: 5 days
+    # frozen means freezer compartment with separate doors: 3-6 months
+    # thawed milk needs to be used in 24 hours - expiration date is today;
+    case milk_type
+      when 'fresh'
+        return date.advance(days: 5)
+      when 'frozen'
+        return date.advance(months: 6)
+      when 'thawed'
+        return date
+    end
+  end
 end
